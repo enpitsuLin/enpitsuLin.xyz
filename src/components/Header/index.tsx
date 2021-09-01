165147a66356c9e60ce7d95ac47dd16ea1178494
enpitsuLin <lldslm@vip.qq.com>
2021-09-02 00:47:00 +0800

    feat: truly hello world

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import "./style.scss"
 
 interface Props {
   siteTitle?: string
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,9 +7,7 @@ import Seo from "../components/Seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>Hello World!</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
