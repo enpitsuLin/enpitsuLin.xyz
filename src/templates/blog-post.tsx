5d998b44536da840b15143250b08e77037c0984e
enpitsuLin <lldslm@vip.qq.com>
2021-09-02 21:24:10 +0800

    feat: 使用全局样式并添加了字体

---
+++ src/templates/blog-post.tsx
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { Link, graphql } from 'gatsby';
+
+import Layout from '../components/Layout';
+import Seo from '../components/seo';
+
+const BlogPostTemplate = ({ data, location }) => {
+  const post = data.markdownRemark;
+  const { previous, next } = data;
+
+  return (
+    <Layout location={location}>
+      <Seo title={post.frontmatter.title} description={post.frontmatter.description || post.excerpt} />
+      <article className="post" itemScope itemType="http://schema.org/Article">
+        <header>
+          <h1 className="post-title" itemProp="headline">
+            {post.frontmatter.title}
+          </h1>
+        </header>
+        <div className="post-meta">
+          <i className="fa fa-calendar" aria-hidden="true"></i>
+          <span>{post.frontmatter.date}</span>
+        </div>
+        <div className="post-content" dangerouslySetInnerHTML={{ __html: post.html }} itemProp="articleBody" />
+        <nav className="post-nav">
+          {previous && (
+            <Link to={previous.fields.slug} rel="prev">
+              ← {previous.frontmatter.title}
+            </Link>
+          )}
+
+          {next && (
+            <Link to={next.fields.slug} rel="next">
+              {next.frontmatter.title} →
+            </Link>
+          )}
+        </nav>
+      </article>
+    </Layout>
+  );
+};
+
+export default BlogPostTemplate;
+
+export const pageQuery = graphql`
+  query BlogPostBySlug($id: String!, $previousPostId: String, $nextPostId: String) {
+    site {
+      siteMetadata {
+        title
+        description
+      }
+    }
+    markdownRemark(id: { eq: $id }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "YYYY 年 MM月 DD日 ")
+        description
+      }
+    }
+    previous: markdownRemark(id: { eq: $previousPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+    next: markdownRemark(id: { eq: $nextPostId }) {
+      fields {
+        slug
+      }
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
