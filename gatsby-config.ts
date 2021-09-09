4a2901c69f3a9acbef5f47928089dc5b0533f688
enpitsuLin <lldslm@vip.qq.com>
2021-09-09 21:35:58 +0800

    fix: 禁用了 `gatsby-plugin-manifest`

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -149,7 +149,7 @@ const plugins = [
       ]
     }
   },
-  {
+ /*  {
     resolve: `gatsby-plugin-manifest`,
     options: {
       name: `enpitsuLin's Blog`,
@@ -160,7 +160,7 @@ const plugins = [
       display: `minimal-ui`,
       icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
     }
-  },
+  }, */
   `gatsby-plugin-react-helmet`,
   `gatsby-plugin-gatsby-cloud`,
   // this (optional) plugin enables Progressive Web App + Offline functionality
