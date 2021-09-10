f23fcbf8838d9444290620e286ff977a368a84d3
enpitsulin <lldslm@vip.qq.com>
2021-09-10 11:51:02 +0800

    feat: 使用组件库替代自制组件

--- src/components/Article/ArticleItemList/index.tsx
+++ src/components/Article/ArticleItemList/index.tsx
@@ -1,7 +1,8 @@
-import Pagination from '@/components/Pagination';
+// import Pagination from '@/components/Pagination';
 import { navigateToArticle } from '@/utils/article';
 import React, { FunctionComponent } from 'react';
 import ArticleItem from './ListItem';
+import Pagination from 'react-bootstrap/Pagination';
 
 interface Props {
   articles: GatsbyTypes.MarkdownRemark[];
@@ -16,13 +17,30 @@ const ArticleItemList: FunctionComponent<Props> = ({ articles, pageCount, pageIn
         <ArticleItem key={index} article={article}></ArticleItem>
       ))}
       <div className="flex justify-center">
-        <Pagination
+        <Pagination>
+          <Pagination.First />
+          <Pagination.Prev />
+          <Pagination.Ellipsis />
+          {Array.from({ length: pageCount }).map((_, index) => (
+            <Pagination.Item
+              onClick={() => {
+                navigateToArticle(index === 0 ? '' : `/${index + 1}`);
+              }}
+            >
+              {index + 1}
+            </Pagination.Item>
+          ))}
+          <Pagination.Ellipsis />
+          <Pagination.Next />
+          <Pagination.Last />
+        </Pagination>
+        {/* <Pagination
           pageCount={pageCount}
           currentPage={pageIndex + 1}
           onChange={toPage => {
             navigateToArticle(toPage === 1 ? '' : `/${toPage}`);
           }}
-        />
+        /> */}
       </div>
     </div>
   );
