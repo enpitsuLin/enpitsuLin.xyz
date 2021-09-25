14ebcfa7677f00e90658d27c9de09cd8e94508da
enpitsuLin <lldslm@vip.qq.com>
2021-09-25 18:01:46 +0800

    feat: add Contacts components

---
+++ src/components/Contacts.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Box, BoxProps, Icon } from '@chakra-ui/react';
+import { FaQq, FaMailBulk, FaGithub, FaSteam, FaZhihu } from 'react-icons/fa';
+
+const contacts = [
+  [FaQq, 'http://wpa.qq.com/msgrd?v=3&uin=109299651&site=qq&menu=yes', 'QQ: 109299651'],
+  [FaMailBulk, 'mailto://enpitsulin@gmail.com', 'E-mail: enpitsulin@gmail.com'],
+  [FaGithub, 'https://github.com/enpitsulin', 'GitHub: enpitsulin'],
+  [FaSteam, 'https://steamcommunity.com/profiles/76561198338250608/', 'Steam: promise'],
+  [FaZhihu, 'https://zhihu.com/people/enpitsulin', '知乎：enpitsulin']
+] as Array<[React.ComponentType, string, string]>;
+
+const Contacts: React.FC<BoxProps> = props => {
+  return (
+    <Box {...props}>
+      {contacts.map(contact => {
+        const [icon, link, title] = contact;
+        return (
+          <a href={link} title={title} target="__blank">
+            <Icon
+              as={icon}
+              transition="transform 0.2s linear 0s"
+              mx={4}
+              h={6}
+              w={6}
+              _hover={{ transform: 'scale(1.4)' }}
+            />
+          </a>
+        );
+      })}
+    </Box>
+  );
+};
+
+export default Contacts;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,17 @@
 import React, { FunctionComponent, useEffect, useState } from 'react';
 import { PageRendererProps } from 'gatsby';
+import { Box, Flex, Text, Icon } from '@chakra-ui/react';
+import { FaClock } from 'react-icons/fa';
+
+import useSiteMetadata from '@/hooks/useSiteMetadata';
 import { BasicLayout } from '@/layouts/';
 import Seo from '@/components/seo';
 import useTypeWriter from '@/hooks/useTypewriter';
 import AnimatedContent from '@/components/AnimatedContent';
-import { Box, Flex, Text } from '@chakra-ui/layout';
+
 import CanvasNest from '@/components/CanvasNest';
+import dayjs from 'dayjs';
+import Contacts from '@/components/Contacts';
 
 type Props = PageRendererProps;
 
@@ -14,6 +20,9 @@ const maxims = ['Sow nothing, reap noting', 'Do what you love, Love what you do'
 const BlogIndex: FunctionComponent<Props> = ({ location }) => {
   const [maxim, setMaxim] = useState(maxims[0]);
   const text = useTypeWriter(maxim);
+  const { site } = useSiteMetadata();
+  const lastUpdateTime = dayjs(site?.siteMetadata?.lastUpdateTime || '');
+
   useEffect(() => {
     const timer = setInterval(() => {
       setMaxim(maxims[Math.floor(Math.random() * maxims.length)]);
@@ -33,8 +42,17 @@ const BlogIndex: FunctionComponent<Props> = ({ location }) => {
                 <Text fontSize="3rem" fontWeight="500" py={1} my={3}>
                   你好
                 </Text>
-                <Text as="div" py={1} my={2} h={6}>
+                <Text as="p" py={1} my={2} h={6}>
                   <span>{text || ' '}</span>
+                </Text>
+                <Box py={1} my={2}></Box>
+                <Contacts mt={2} mb={4} />
+                <Text as="p" py={1} my={2}>
+                  <Icon as={FaClock} mt="-3px" mr={1} />
+                  最后更新
+                  <Text as="strong" mx="1">
+                    {lastUpdateTime.format('YYYY-MM-DD[T]HH:mm:ssZ[Z]')}
+                  </Text>
                 </Text>
               </Box>
             </Flex>
