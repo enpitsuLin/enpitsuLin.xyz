590ccc230155780e9fe4b17986cf7ac135be1179
enpitsulin <lldslm@vip.qq.com>
2021-10-31 15:51:11 +0800

    fix: remove prismjs' style import

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import './src/styles/index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import DefaultTheme from './src/theme';
-import 'prismjs/plugins/command-line/prism-command-line.css';
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 
 export const wrapRootElement = props => {
   const { element } = props;
