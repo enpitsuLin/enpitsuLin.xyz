7a59f1b9b0c5ee90c3ce27bc3206246f2804ef0e
enpitsulin <lldslm@vip.qq.com>
2021-11-03 17:41:09 +0800

    feat: upgrade dependency

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './src/styles/index.css';
 import { ChakraProvider } from '@chakra-ui/react';
 import DefaultTheme from './src/theme';
+import '@enpitsulin/gatsby-remark-shiki/style/toolbar.css';
 
 export const wrapRootElement = props => {
   const { element } = props;
