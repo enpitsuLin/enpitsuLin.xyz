18451c6b9fb2eb42f486f340162fcfce4e67a735
enpitsulin <lldslm@vip.qq.com>
2021-11-24 20:17:25 +0800

    feat: change chakra default colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -5,6 +5,21 @@ const config = {
   initialColorMode: 'dark'
 };
 
+const colors = {
+  gray: {
+    50: '#f7f7f7',
+    100: '#ededed',
+    200: '#e2e2e2',
+    300: '#cbcbcb',
+    400: '#a0a0a0',
+    500: '#717171',
+    600: '#4a4a4a',
+    700: '#2d2d2d',
+    800: '#1a1a1a',
+    900: '#171717'
+  }
+};
+
 const styles: Styles = {
   global: props => ({
     body: {
@@ -17,9 +32,6 @@ const styles: Styles = {
   })
 };
 
-const DefaultTheme = extendTheme({
-  config,
-  styles
-});
+const DefaultTheme = extendTheme({ config, styles, colors });
 
 export default DefaultTheme;
