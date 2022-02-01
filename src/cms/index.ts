47a51a3c01cdd8cace5bc84b1720bc8296a01f78
enpitsulin <enpitsulin@gmail.com>
2022-02-01 23:19:18 +0800

    feat: netlify locale configruation

--- src/cms/index.ts
+++ src/cms/index.ts
@@ -1,3 +1,5 @@
 import CMS from 'netlify-cms-app';
 import { TagControl, TagPreview } from './widgets/TagWidget';
+import { zh_Hans as cn } from 'netlify-cms-locales';
 CMS.registerWidget('tags', TagControl, TagPreview);
+CMS.registerLocale('cn', cn);
