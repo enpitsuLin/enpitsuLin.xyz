a59bd7e046408f7fe53c7ab6f1b55771a9b7d681
enpitsulin <enpitsulin@gmail.com>
2022-02-02 20:57:01 +0800

    fix: ssr config

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { ColorModeScript } from '@chakra-ui/react';
-import theme from './src/theme';
+import theme from './src/styles/theme';
 
 export const onRenderBody = ({ setPreBodyComponents }) => {
-    setPreBodyComponents([
-      <ColorModeScript
-        initialColorMode={theme.config.initialColorMode}
-        key="chakra-ui-no-flash"
-      />,
-    ])
-  }
-  
+  setPreBodyComponents([<ColorModeScript initialColorMode={theme.config.initialColorMode} key="chakra-ui-no-flash" />]);
+};
+
 export { wrapRootElement } from './gatsby-browser';
