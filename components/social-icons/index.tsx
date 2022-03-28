5fac1db4ae67acdc241c89f33802f713705e4393
enpitsulin <lldslm@vip.qq.com>
2022-03-28 10:05:42 +0800

    feat: social use react-icons

--- components/social-icons/index.tsx
+++ components/social-icons/index.tsx
@@ -1,26 +1,34 @@
-import Mail from './mail.svg'
-import Github from './github.svg'
-import Facebook from './facebook.svg'
-import Youtube from './youtube.svg'
-import Linkedin from './linkedin.svg'
-import Twitter from './twitter.svg'
+import { IconType } from 'react-icons'
+import { FaTwitter, FaEnvelope, FaGithub, FaYoutube, FaFacebook, FaLinkedin } from 'react-icons/fa'
 
-// Icons taken from: https://simpleicons.org/
+type SocialKind = 'mail' | 'github' | 'facebook' | 'youtube' | 'linkedin' | 'twitter'
 
-const components = {
-  mail: Mail,
-  github: Github,
-  facebook: Facebook,
-  youtube: Youtube,
-  linkedin: Linkedin,
-  twitter: Twitter,
+interface SocialIconProps {
+  kind: SocialKind
+  href: string
+  size?: 'small' | 'medium' | 'large'
 }
 
-const SocialIcon = ({ kind, href, size = 8 }) => {
+const components: Record<SocialKind, IconType> = {
+  mail: FaEnvelope,
+  github: FaGithub,
+  facebook: FaFacebook,
+  youtube: FaYoutube,
+  linkedin: FaLinkedin,
+  twitter: FaTwitter,
+}
+const sizeMap = {
+  small: `h-6 w-6`,
+  medium: `h-8 w-8`,
+  large: `h-12 w-12`,
+}
+
+const SocialIcon: React.FC<SocialIconProps> = ({ kind, href, size = 'medium' }) => {
   if (!href || (kind === 'mail' && !/^mailto:\w+([.-]?\w+)@\w+([.-]?\w+)(.\w{2,3})+$/.test(href)))
     return null
 
   const SocialSvg = components[kind]
+  const sizeClass = sizeMap[size]
 
   return (
     <a
@@ -31,7 +39,10 @@ const SocialIcon = ({ kind, href, size = 8 }) => {
     >
       <span className="sr-only">{kind}</span>
       <SocialSvg
-        className={`fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400 h-${size} w-${size}`}
+        className={[
+          `fill-current text-gray-700 hover:text-blue-500 dark:text-gray-200 dark:hover:text-blue-400`,
+          sizeClass,
+        ].join(' ')}
       />
     </a>
   )
