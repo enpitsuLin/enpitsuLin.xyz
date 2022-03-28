915a09cbb88b9da280631c6fa659f04a1d81dbd2
enpitsulin <lldslm@vip.qq.com>
2022-03-28 09:14:59 +0800

    refactor: improve typescript

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,12 @@
-interface Window {
-  disqus_config: any
-  DISQUS: any
-  dataLayer: any[]
-  gtag: (...arguments: any[]) => void
-  plausible: { q: any[]; (...arguments: any[]): void }
-  sa_event: (...arguments: any[]) => void
+declare global {
+  interface Window {
+    disqus_config: any
+    DISQUS: any
+    dataLayer: any[]
+    gtag: (...arguments: any[]) => void
+    plausible: { q: any[]; (...arguments: any[]): void }
+    sa_event: (...arguments: any[]) => void
+  }
 }
 
 declare module 'mdast-util-to-string' {
