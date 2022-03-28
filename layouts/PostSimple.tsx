f77e72228b4f6e629ba37185062c67af2ac7335a
enpitsulin <lldslm@vip.qq.com>
2022-03-28 15:03:40 +0800

    refactor: comment placeholder

--- layouts/PostSimple.tsx
+++ layouts/PostSimple.tsx
@@ -46,6 +46,7 @@ export default function PostLayout({ frontMatter, next, prev, children }: Props)
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
+            <div id="comment"></div>
             <footer>
               <div className="flex flex-col text-sm font-medium sm:flex-row sm:justify-between sm:text-base">
                 {prev && (
