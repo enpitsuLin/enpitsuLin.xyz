74eaf4ecba9f538e938ff9004714b18c991e8e2e
enpitsulin <lldslm@vip.qq.com>
2022-03-28 09:19:45 +0800

    refactor: improve typescript

--- components/analytics/index.tsx
+++ components/analytics/index.tsx
@@ -1,17 +1,8 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import GA from './GoogleAnalytics'
 import Plausible from './Plausible'
 import SimpleAnalytics from './SimpleAnalytics'
 import Umami from './Umami'
 import siteMetadata from '@/data/siteMetadata'
-
-declare global {
-  interface Window {
-    gtag?: (...args: any[]) => void
-    plausible?: (...args: any[]) => void
-    sa_event?: (...args: any[]) => void
-  }
-}
 
 const isProduction = process.env.NODE_ENV === 'production'
 
--- global.d.ts
+++ global.d.ts
@@ -1,12 +1,10 @@
-declare global {
-  interface Window {
-    disqus_config: any
-    DISQUS: any
-    dataLayer: any[]
-    gtag: (...arguments: any[]) => void
-    plausible: { q: any[]; (...arguments: any[]): void }
-    sa_event: (...arguments: any[]) => void
-  }
+interface Window {
+  disqus_config: any
+  DISQUS: any
+  dataLayer: any[]
+  gtag: (...arguments: any[]) => void
+  plausible: { q: any[]; (...arguments: any[]): void }
+  sa_event: (...arguments: any[]) => void
 }
 
 declare module 'mdast-util-to-string' {
