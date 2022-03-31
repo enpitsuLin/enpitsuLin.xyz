59a280c32ef560bc44444f66e3cc741ec4128dc0
enpitsulin <lldslm@vip.qq.com>
2022-03-31 14:44:08 +0800

    feat: some style

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,18 +1,32 @@
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { useRenderCanvas } from './renderCanvas'
 
-const Hero: React.FC = () => {
+interface Props {
+  welcome: string
+}
+
+const Hero: React.FC<Props> = ({ welcome }) => {
   const canvas = useRef<HTMLCanvasElement>()
+  const ref = useRef<HTMLDivElement>(null)
   useRenderCanvas()
+  useEffect(() => {
+    ref.current?.classList.add('transition-in')
+  }, [])
   return (
     <div>
-      <canvas
-        ref={canvas}
-        className="bg-skin-base pointer-events-none absolute inset-0"
-        id="canvas"
-      ></canvas>
+      <canvas className="bg-skin-base pointer-events-none absolute inset-0" id="canvas"></canvas>
       <div className="relative z-10 flex h-[calc(100vh_-_135px)] items-center justify-center">
-        <div className="px-4 text-3xl md:text-4xl pointer-events-none">无语了</div>
+        <div ref={ref} className="px-4 text-3xl md:text-4xl cursor-default">
+          {welcome.split('').map((latter, index) => (
+            <span
+              style={{ transitionDelay: 0.2 * (index + 1) + 's' }}
+              className="opacity-0 transition-opacity"
+              key={index}
+            >
+              {latter}
+            </span>
+          ))}
+        </div>
       </div>
     </div>
   )
