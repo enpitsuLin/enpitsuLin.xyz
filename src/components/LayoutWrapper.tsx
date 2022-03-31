a0eb0795cb76095540de92074950914bc1665e75
enpitsulin <lldslm@vip.qq.com>
2022-03-31 16:20:24 +0800

    feat: header effect

--- src/components/LayoutWrapper.tsx
+++ src/components/LayoutWrapper.tsx
@@ -5,30 +5,45 @@ import SectionContainer from './SectionContainer'
 import Footer from './Footer'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
-import { ReactNode } from 'react'
+import { useEffect, useRef, useState } from 'react'
 
-interface Props {
-  children: ReactNode
-}
+const LayoutWrapper: React.FC = ({ children }) => {
+  const [stuck, setStuck] = useState(false)
+  const ref = useRef<HTMLHeadElement>()
+  const stuckClasses =
+    'py-2 md:py-5 sticky -top-1 z-50 transition-all backdrop-filter backdrop-blur-md mx-auto border-b border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+  const unstuckClasses =
+    'py-2 md:py-8 sticky -top-1 z-50 transition-all backdrop-filter backdrop-blur-md mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
 
-const LayoutWrapper = ({ children }: Props) => {
+  const classes = stuck ? stuckClasses : unstuckClasses
+
+  useEffect(() => {
+    const cachedRef = ref.current
+    const observer = new IntersectionObserver(
+      ([e]) => {
+        setStuck(e.intersectionRatio < 1)
+      },
+      { threshold: [1.0] }
+    )
+    observer.observe(cachedRef)
+    return () => observer.unobserve(cachedRef)
+  }, [ref])
   return (
-    <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
-              </div>
-            </Link>
-          </div>
+    <>
+      <header className={classes} ref={ref}>
+        <div className="flex justify-between items-center max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+          <Link href="/" aria-label={siteMetadata.headerTitle}>
+            <div className="flex items-center justify-between">
+              {typeof siteMetadata.headerTitle === 'string' ? (
+                <div className="h-6 text-2xl font-semibold sm:block">
+                  {siteMetadata.headerTitle}
+                </div>
+              ) : (
+                siteMetadata.headerTitle
+              )}
+            </div>
+          </Link>
+
           <div className="flex items-center text-base leading-5">
             <div className="hidden sm:block">
               {headerNavLinks.map((link) => (
@@ -44,11 +59,15 @@ const LayoutWrapper = ({ children }: Props) => {
             <ThemeSwitch />
             <MobileNav />
           </div>
-        </header>
-        <main className="mb-auto">{children}</main>
-        <Footer />
-      </div>
-    </SectionContainer>
+        </div>
+      </header>
+      <SectionContainer>
+        <div className="flex h-screen flex-col justify-between">
+          <main className="mb-auto">{children}</main>
+          <Footer />
+        </div>
+      </SectionContainer>
+    </>
   )
 }
 
