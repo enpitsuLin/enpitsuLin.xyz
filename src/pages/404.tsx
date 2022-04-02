f72432f78e9dbbbfe60d6364cf7a43283ee6c62a
enpitsulin <lldslm@vip.qq.com>
2022-04-02 15:43:19 +0800

    feat: add 404 page translation

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,9 @@
+import useTranslation from 'next-translate/useTranslation'
 import Link from '@/components/Link'
 
 export default function FourZeroFour() {
+  const { t } = useTranslation('common')
+
   return (
     <div className="flex flex-col items-start justify-start md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6">
       <div className="space-x-2 pt-6 pb-8 md:space-y-5">
@@ -10,12 +13,12 @@ export default function FourZeroFour() {
       </div>
       <div className="max-w-md">
         <p className="mb-4 text-xl font-bold leading-normal md:text-2xl">
-          Sorry we couldn't find this page.
+          {t('exception.not-found.title')}
         </p>
-        <p className="mb-8">But dont worry, you can find plenty of other things on our homepage.</p>
+        <p className="mb-8">{t('exception.not-found.desc')}</p>
         <Link href="/">
           <button className="focus:shadow-outline-blue inline rounded-lg border border-transparent bg-blue-600 px-4 py-2 text-sm font-medium leading-5 text-white shadow transition-colors duration-150 hover:bg-blue-700 focus:outline-none dark:hover:bg-blue-500">
-            Back to homepage
+            {t('exception.not-found.back')}
           </button>
         </Link>
       </div>
