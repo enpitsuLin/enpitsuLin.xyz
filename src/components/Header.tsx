f27e89ef337484732038c5dd89a4bd7ad812d159
enpitsulin <lldslm@vip.qq.com>
2022-04-11 09:34:05 +0800

    refactor: make header shorter

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,9 +13,9 @@ const Header = () => {
   const [stuck, setStuck] = useState(false)
   const ref = useRef<HTMLHeadElement>()
   const stuckClasses =
-    'py-2 md:py-5 sticky -top-1 z-50 transition-all backdrop-filter backdrop-blur-md mx-auto border-b border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+    'py-2 md:py-3 sticky -top-1 z-50 transition-all backdrop-filter backdrop-blur-md mx-auto border-b border-slate-900/10 dark:border-slate-300/10 w-full'
   const unstuckClasses =
-    'py-2 md:py-8 sticky -top-1 z-50 transition-all mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 mb-16 w-full'
+    'py-2 md:py-6 sticky -top-1 z-50 transition-all mx-auto border-b border-b-0 border-slate-900/10 dark:border-slate-300/10 w-full'
 
   const classes = stuck ? stuckClasses : unstuckClasses
 
