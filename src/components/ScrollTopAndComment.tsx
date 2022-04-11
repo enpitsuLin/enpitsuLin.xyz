36480b261169f4078966f65c7d1990f5ee249431
enpitsulin <lldslm@vip.qq.com>
2022-04-11 11:32:23 +0800

    feat: impove Toc nav

--- src/components/ScrollTopAndComment.tsx
+++ src/components/ScrollTopAndComment.tsx
@@ -1,14 +1,28 @@
+import { Toc } from '@/types/Toc'
 import { useEffect, useState } from 'react'
+import { FaArrowUp, FaComment, FaList } from 'react-icons/fa'
 
-const ScrollTopAndComment = () => {
+const Button: React.FC<JSX.IntrinsicElements['button']> = ({ children, ...rest }) => {
+  const classes = [
+    rest.className,
+    'rounded-full bg-gray-200 p-2 text-gray-500 transition-all hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600',
+  ].join(' ')
+  return (
+    <button {...rest} type="button" className={classes}>
+      {children}
+    </button>
+  )
+}
+
+const ScrollTopAndComment: React.FC<{ toc: Toc }> = ({ toc }) => {
   const [show, setShow] = useState(false)
+  const [tocShow, setTocShow] = useState(false)
 
   useEffect(() => {
     const handleWindowScroll = () => {
       if (window.scrollY > 50) setShow(true)
       else setShow(false)
     }
-
     window.addEventListener('scroll', handleWindowScroll)
     return () => window.removeEventListener('scroll', handleWindowScroll)
   }, [])
@@ -19,38 +33,56 @@ const ScrollTopAndComment = () => {
   const handleScrollToComment = () => {
     document.getElementById('comment').scrollIntoView()
   }
+  const handleScrollToToc = (selector: string) => {
+    const el = document.querySelector(selector) as HTMLElement
+    const { offsetTop } = el
+    window.scrollTo({
+      top: offsetTop < 1000 ? offsetTop - 69 : offsetTop,
+      behavior: 'smooth',
+    })
+  }
   return (
-    <div
-      className={`fixed right-8 bottom-8 hidden flex-col gap-3 ${show ? 'md:flex' : 'md:hidden'}`}
-    >
-      <button
-        aria-label="Scroll To Comment"
-        type="button"
-        onClick={handleScrollToComment}
-        className="rounded-full bg-gray-200 p-2 text-gray-500 transition-all hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
-      >
-        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M18 10c0 3.866-3.582 7-8 7a8.841 8.841 0 01-4.083-.98L2 17l1.338-3.123C2.493 12.767 2 11.434 2 10c0-3.866 3.582-7 8-7s8 3.134 8 7zM7 9H5v2h2V9zm8 0h-2v2h2V9zM9 9h2v2H9V9z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-      <button
+    <div className="fixed right-8 bottom-8 hidden flex-col gap-3 md:flex z-20">
+      <Button aria-label="Scroll To Comment" onClick={handleScrollToComment.bind(this)}>
+        <FaComment className="h-4 w-4" />
+      </Button>
+      {toc.length > 0 && (
+        <div className="relative">
+          <Button
+            aria-label="Table of content"
+            onClick={() => {
+              setTocShow((show) => !show)
+            }}
+          >
+            <FaList className="h-4 w-4" />
+          </Button>
+          {tocShow && (
+            <div className="absolute right-10 bottom-0 h-80 w-80 rounded-md shadow-md p-4 overflow-y-auto">
+              <div className="flex flex-col">
+                {toc.map((h) => (
+                  <a
+                    key={h.value}
+                    className="hover:bg-gray-500 cursor-pointer my-1 px-1"
+                    style={{ marginLeft: `${(h.depth - 1) * 20}px` }}
+                    onClick={() => {
+                      handleScrollToToc(h.url)
+                    }}
+                  >
+                    <span>{h.value}</span>
+                  </a>
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+      <Button
         aria-label="Scroll To Top"
-        type="button"
+        className={show ? 'md:block' : 'md:hidden'}
         onClick={handleScrollTop}
-        className="rounded-full bg-gray-200 p-2 text-gray-500 transition-all hover:bg-gray-300 dark:bg-gray-700 dark:text-gray-400 dark:hover:bg-gray-600"
       >
-        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-          <path
-            fillRule="evenodd"
-            d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
+        <FaArrowUp className="h-4 w-4" />
+      </Button>
     </div>
   )
 }
