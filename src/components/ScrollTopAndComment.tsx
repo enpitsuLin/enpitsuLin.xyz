354e0a950f3d3aa50a4dd2d69e033c9a73add9f8
enpitsulin <lldslm@vip.qq.com>
2022-04-14 15:18:48 +0800

    feat: improve TOC

--- src/components/ScrollTopAndComment.tsx
+++ src/components/ScrollTopAndComment.tsx
@@ -1,6 +1,7 @@
 import { Toc } from '@/types/Toc'
 import { useEffect, useState } from 'react'
 import { FaArrowUp, FaComment, FaList } from 'react-icons/fa'
+import TOCInline from './TOCInline'
 
 const Button: React.FC<JSX.IntrinsicElements['button']> = ({ children, ...rest }) => {
   const classes = [
@@ -33,14 +34,6 @@ const ScrollTopAndComment: React.FC<{ toc: Toc }> = ({ toc }) => {
   const handleScrollToComment = () => {
     document.getElementById('comment').scrollIntoView()
   }
-  const handleScrollToToc = (selector: string) => {
-    const el = document.querySelector(selector) as HTMLElement
-    const { offsetTop } = el
-    window.scrollTo({
-      top: offsetTop < 1000 ? offsetTop - 69 : offsetTop,
-      behavior: 'smooth',
-    })
-  }
   return (
     <div className="fixed right-8 bottom-8 hidden flex-col gap-3 md:flex z-20">
       <Button aria-label="Scroll To Comment" onClick={handleScrollToComment}>
@@ -57,20 +50,9 @@ const ScrollTopAndComment: React.FC<{ toc: Toc }> = ({ toc }) => {
             <FaList className="h-4 w-4" />
           </Button>
           {tocShow && (
-            <div className="absolute right-10 bottom-0 h-80 w-80 bg-gray-200 dark:bg-gray-700 shadow rounded-md p-4 overflow-y-auto">
-              <div className="flex flex-col">
-                {toc.map((h) => (
-                  <a
-                    key={h.value}
-                    className="hover:bg-gray-100 dark:hover:bg-gray-500 cursor-pointer my-1 px-2"
-                    style={{ marginLeft: `${(h.depth - 1) * 20}px` }}
-                    onClick={() => {
-                      handleScrollToToc(h.url)
-                    }}
-                  >
-                    <span>{h.value}</span>
-                  </a>
-                ))}
+            <div className="absolute right-10 bottom-0 w-80 max-h-180 bg-gray-200 dark:bg-gray-700 shadow p-2 rounded-md overflow-y-auto">
+              <div className="prose dark:prose-dark">
+                <TOCInline toc={toc} asDisclosure />
               </div>
             </div>
           )}
--- src/components/TOCInline.tsx
+++ src/components/TOCInline.tsx
@@ -1,4 +1,5 @@
 import { Toc } from '@/types/Toc'
+import useTranslation from 'next-translate/useTranslation'
 
 interface TOCInlineProps {
   toc: Toc
@@ -24,28 +25,44 @@ interface TOCInlineProps {
  * }
  *
  */
-const TOCInline = ({
+const TOCInline: React.FC<TOCInlineProps> = ({
   toc,
   indentDepth = 3,
   fromHeading = 1,
   toHeading = 6,
   asDisclosure = false,
   exclude = '',
-}: TOCInlineProps) => {
+}) => {
   const re = Array.isArray(exclude)
     ? new RegExp('^(' + exclude.join('|') + ')$', 'i')
     : new RegExp('^(' + exclude + ')$', 'i')
+
+  const { t } = useTranslation('common')
 
   const filteredToc = toc.filter(
     (heading) =>
       heading.depth >= fromHeading && heading.depth <= toHeading && !re.test(heading.value)
   )
-
+  const handleScrollToToc = (selector: string) => {
+    const el = document.querySelector(selector) as HTMLElement
+    const { offsetTop } = el
+    window.scrollTo({
+      top: offsetTop < 1000 ? offsetTop - 69 : offsetTop,
+      behavior: 'smooth',
+    })
+  }
   const tocList = (
     <ul>
       {filteredToc.map((heading) => (
         <li key={heading.value} className={`${heading.depth >= indentDepth && 'ml-6'}`}>
-          <a href={heading.url}>{heading.value}</a>
+          <span
+            onClick={() => {
+              handleScrollToToc(heading.url)
+            }}
+            className="cursor-pointer"
+          >
+            {heading.value}
+          </span>
         </li>
       ))}
     </ul>
@@ -55,7 +72,7 @@ const TOCInline = ({
     <>
       {asDisclosure ? (
         <details open>
-          <summary className="ml-6 pt-2 pb-2 text-xl font-bold">Table of Contents</summary>
+          <summary className="ml-6 pt-2 pb-2 text-xl font-bold">{t('toc')}</summary>
           <div className="ml-6">{tocList}</div>
         </details>
       ) : (
