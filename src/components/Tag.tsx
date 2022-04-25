7f336b91cc7775a81e84336ad58b2f4ed2ca70a2
enpitsulin <lldslm@vip.qq.com>
2022-04-25 09:50:24 +0800

    refactor: tag lowercase

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -8,8 +8,8 @@ interface Props {
 const Tag = ({ text }: Props) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
-        {text.split(' ').join('-')}
+      <a className="mr-3 text-md font-medium lowercase text-primary-500 hover:text-primary-600 dark:hover:text-primary-400">
+        &#x23;{text.split(' ').join('-')}
       </a>
     </Link>
   )
