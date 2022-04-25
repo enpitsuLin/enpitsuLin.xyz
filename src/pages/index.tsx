548c2bbdc88cd275b03d1674c5347bcc149fbe26
enpitsulin <lldslm@vip.qq.com>
2022-04-25 11:31:18 +0800

    feat: add translate

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,7 +26,7 @@ export default function Home({ posts }: InferGetStaticPropsType<typeof getStatic
         <Hero welcome={t('welcome')} />
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Latest
+            {t('latest')}
           </h1>
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
