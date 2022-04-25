933b5333ddd226324195b9d5a85064ddebc6e9a5
enpitsulin <lldslm@vip.qq.com>
2022-04-25 10:43:51 +0800

    refactor: max-width in SectionContainer

--- src/components/SectionContainer.tsx
+++ src/components/SectionContainer.tsx
@@ -1,4 +1,4 @@
 const SectionContainer: React.FC = ({ children }) => {
-  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-5xl xl:px-0">{children}</div>
+  return <div className="mx-auto max-w-3xl px-4 sm:px-6 xl:max-w-6xl xl:px-0">{children}</div>
 }
 export default SectionContainer
