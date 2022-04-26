404e1a15f14262787aa1fc53bbcd52c6778bcc6a
enpitsulin <enpitsulin@gmail.com>
2022-04-26 11:28:31 +0800

    refactor: tags page

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -1,7 +1,6 @@
 import useTranslation from 'next-translate/useTranslation'
 import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
-import Tag from '@/components/Tag'
 import siteMetadata from 'data/siteMetadata'
 import { getAllTags } from '@/lib/tags'
 import kebabCase from '@/lib/utils/kebabCase'
@@ -19,23 +18,32 @@ export default function Tags({ tags }: InferGetStaticPropsType<typeof getStaticP
   return (
     <>
       <PageSEO title={`Tags - ${siteMetadata.author}`} description="Things I blog about" />
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
+      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700">
+        <div className="pt-6 pb-8 space-x-2 md:space-y-5">
+          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             {t('header.tags')}
           </h1>
         </div>
-        <div className="flex max-w-lg flex-wrap">
+
+        <div className="flex flex-wrap w-full">
           {Object.keys(tags).length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
             return (
-              <div key={t} className="mt-2 mb-2 mr-5">
-                <Tag text={t} />
+              <div
+                key={t}
+                className="inline-flex flex-row items-center relative rounded border border-gray-700 hover:border-accent font-medium mt-2 mb-2 mr-5 bg-day dark:bg-night bg-opacity-50 dark:bg-opacity-50"
+              >
                 <Link
                   href={`/tags/${kebabCase(t)}`}
-                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  className="px-2 py-1 text-sm font-medium hover:text-primary-600 dark:hover:text-primary-400"
                 >
-                  {` (${tags[t]})`}
+                  {t}
+                </Link>
+                <Link
+                  href={`/tags/${kebabCase(t)}`}
+                  className="inline-flex items-center h-full border-gray-700 border-l px-2 bg-gray-300 rounded-r dark:bg-gray-800 text-sm font-semibold"
+                >
+                  {tags[t]}
                 </Link>
               </div>
             )
