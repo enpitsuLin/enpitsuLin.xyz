38a6dca4e20fad6a1a4febaab084bc019b2e76f5
enpitsulin <enpitsulin@gmail.com>
2022-04-26 11:09:25 +0800

    refactor: remove about route

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,6 @@ const headerNavLinks = [
   { href: '/', title: 'header.home' },
   { href: '/blog', title: 'header.blog' },
   { href: '/tags', title: 'header.tags' },
-  { href: '/about', title: 'header.about' },
 ]
 
 export default headerNavLinks
--- src/pages/about.tsx
+++
@@ -1,4 +0,0 @@
-const About = () => {
-  return <div></div>
-}
-export default About
