bcb8e7dae7dcbc72d326eb79f707eeb046510981
enpitsulin <enpitsulin@gmail.com>
2022-04-27 11:34:02 +0800

    Revert "feat: add ackee"
    This reverts commit 52c1fcaa724fe2906248b2a8d547e17f6f3373ee.

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -22,7 +22,6 @@ const siteMetadata = {
     simpleAnalytics: false,
     umamiWebsiteId: '',
     googleAnalyticsId: 'G-YB5GQDJZ4R',
-    ackeeDomainId: process.env.NEXT_PUBLIC_ACKEE_DOMAIN_ID,
   },
   lastUpdateTime: new Date(),
 }
--- src/components/analytics/index.tsx
+++ src/components/analytics/index.tsx
@@ -3,7 +3,6 @@ import Plausible from './Plausible'
 import SimpleAnalytics from './SimpleAnalytics'
 import Umami from './Umami'
 import siteMetadata from 'data/siteMetadata'
-import AckeeTracker from './Ackee'
 
 const isProduction = process.env.NODE_ENV === 'production'
 
@@ -14,7 +13,6 @@ const Analytics = () => {
       {isProduction && siteMetadata.analytics.simpleAnalytics && <SimpleAnalytics />}
       {isProduction && siteMetadata.analytics.umamiWebsiteId && <Umami />}
       {isProduction && siteMetadata.analytics.googleAnalyticsId && <GA />}
-      {isProduction && siteMetadata.analytics.ackeeDomainId && <AckeeTracker />}
     </>
   )
 }
