90ac88017bda0f2d4910100e4056daab34831af7
enpitsulin <enpitsulin@gmail.com>
2022-04-28 11:05:58 +0800

    feat: sitemap generate by page

---
+++ src/pages/sitemap.tsx
@@ -0,0 +1,73 @@
+import matter from 'gray-matter'
+import globby from 'globby'
+import prettier from 'prettier'
+import fs from 'fs'
+import siteMetadata from 'data/siteMetadata'
+import { GetStaticProps } from 'next'
+
+async function generateSiteMap() {
+  const prettierConfig = await prettier.resolveConfig('./.prettierrc.js')
+  const pages = await globby([
+    'pages/*.js',
+    'pages/*.tsx',
+    'data/blog/**/*.mdx',
+    'data/blog/**/*.md',
+    'public/tags/**/*.xml',
+    '!pages/_*.js',
+    '!pages/_*.tsx',
+    '!pages/api',
+  ])
+
+  const sitemap = `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            ${pages
+              .map((page) => {
+                // Exclude drafts from the sitemap
+                if (page.search('.md') >= 1 && fs.existsSync(page)) {
+                  const source = fs.readFileSync(page, 'utf8')
+                  const fm = matter(source)
+                  if (fm.data.draft) {
+                    return
+                  }
+                  if (fm.data.canonicalUrl) {
+                    return
+                  }
+                }
+                const path = page
+                  .replace('pages/', '/')
+                  .replace('data/blog', '/blog')
+                  .replace('public/', '/')
+                  .replace('.js', '')
+                  .replace('.tsx', '')
+                  .replace('.mdx', '')
+                  .replace('.md', '')
+                  .replace('/feed.xml', '')
+                const route = path === '/index' ? '' : path
+                if (page.search('pages/404.') > -1 || page.search(`pages/blog/[...slug].`) > -1) {
+                  return
+                }
+                return `
+                        <url>
+                            <loc>${siteMetadata.siteUrl}${route}</loc>
+                        </url>
+                    `
+              })
+              .join('')}
+        </urlset>
+    `
+
+  const formatted = prettier.format(sitemap, {
+    ...prettierConfig,
+    parser: 'html',
+  })
+  return formatted
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const siteMap = await generateSiteMap()
+  fs.writeFileSync('public/sitemap.xml', siteMap)
+  return { props: {}, redirect: { destination: 'sitemap.xml', permanent: true } }
+}
+
+export default function SiteMap() {}
