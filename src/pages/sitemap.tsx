9a2b91a26b806d96ae1f1f47e7eacf326738750b
enpitsulin <enpitsulin@gmail.com>
2022-04-28 14:28:35 +0800

    feat: sitemap lastmod

--- src/pages/sitemap.tsx
+++ src/pages/sitemap.tsx
@@ -3,13 +3,18 @@ import siteMetadata from 'data/siteMetadata'
 import { GetServerSideProps } from 'next'
 import { getAllTags } from '@/lib/tags'
 import { getAllFilesFrontMatter } from '@/lib/mdx'
+import dayjs from 'dayjs'
+
+const lastmod = dayjs().format('YYYY-MM-DD')
 
 async function generateSiteMap() {
   const allPosts = await getAllFilesFrontMatter()
   const tags = Object.keys(await getAllTags())
   const prettierConfig = await prettier.resolveConfig('/prettier.config.js')
 
+  const rootPage = ['', '/']
   const allPages = [
+    ...rootPage,
     ...allPosts.map((post) => {
       return `/blog/${post.slug}`
     }),
@@ -20,12 +25,13 @@ async function generateSiteMap() {
 
   const sitemap = `
         <?xml version="1.0" encoding="UTF-8"?>
-        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
             ${allPages
               .map((page) => {
                 return `
                         <url>
                             <loc>${siteMetadata.siteUrl}${page}</loc>
+                            <lastmod>${lastmod}</lastmod>
                         </url>
                     `
               })
