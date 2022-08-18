85d936760c8a1851a231a84e86db475661ae7fa7
enpitsulin <enpitsulin@gmail.com>
2022-08-18 16:42:52 +0800

    chore: remove preact

--- next.config.js
+++ next.config.js
@@ -44,16 +44,6 @@ const config = {
       use: ['@svgr/webpack'],
     })
 
-    if (!dev && !isServer) {
-      // Replace React with Preact only in client production build
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
     return config
   },
 }
