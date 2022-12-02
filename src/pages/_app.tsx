3dd8b0df47799cdaeff1b5f03c464378b603bf69
enpitsulin <enpitsulin@gmail.com>
2022-12-02 11:36:12 +0800

    feat: use @next/font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,6 @@ import '@/css/prism.css'
 import '@/css/disqusjs.css'
 import 'katex/dist/katex.css'
 
-import '@fontsource/inter/variable-full.css'
-
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
@@ -14,6 +12,9 @@ import siteMetadata from 'data/siteMetadata'
 import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
+import { Inter } from '@next/font/google'
+const inter = Inter({})
+
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
@@ -21,6 +22,11 @@ const App = ({ Component, pageProps }: AppProps) => {
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
       <Analytics />
+      <style jsx global>{`
+        html: {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
