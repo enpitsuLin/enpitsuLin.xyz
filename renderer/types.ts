256cb41a5045e14352fee68af5067539646feb6a
enpitsulin <enpitsulin@gmail.com>
2022-12-06 10:54:59 +0800

    perf: improved usePageContext

--- renderer/types.ts
+++ renderer/types.ts
@@ -1,11 +1,29 @@
-export type PageProps = {};
-// The `pageContext` that are available in both on the server-side and browser-side
-export type PageContext = {
-  Page: (pageProps: PageProps) => React.ReactElement;
-  pageProps: PageProps;
-  urlPathname: string;
+export type { PageContextServer }
+export type { PageContextClient }
+export type { PageContext }
+export type { PageProps }
+
+import type { PageContextBuiltIn } from 'vite-plugin-ssr'
+import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client/router' // When using Client Routing
+// import type { PageContextBuiltInClient } from 'vite-plugin-ssr/client' // When using Server Routing
+
+type Page = (pageProps: PageProps) => React.ReactElement
+type PageProps = Record<string, unknown>
+
+export type PageContextCustom = {
+  Page: Page
+  pageProps?: PageProps
+  exports: {
+    documentProps?: {
+      title: string
+    }
+  }
   documentProps?: {
-    title?: string;
-    description?: string;
-  };
-};
+    title: string
+  }
+}
+
+type PageContextServer = PageContextBuiltIn<Page> & PageContextCustom
+type PageContextClient = PageContextBuiltInClient<Page> & PageContextCustom
+
+type PageContext = PageContextClient | PageContextServer
