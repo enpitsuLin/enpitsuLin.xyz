d25f2805a0994c5da6b99addcbe737c7ca9095d9
enpitsulin <enpitsulin@gmail.com>
2022-12-07 14:59:39 +0800

    build: add bundle analyze

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,33 @@
 import { defineConfig } from 'vite';
+import * as path from 'path';
+
+import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 import react from '@vitejs/plugin-react-swc';
 import ssr from 'vite-plugin-ssr/plugin';
 import vercel from 'vite-plugin-vercel';
-import vercelSsr from '@magne4000/vite-plugin-vercel-ssr';
 import WindiCSS from 'vite-plugin-windicss';
-import Icon from 'unplugin-icons/vite';
-import * as path from 'path';
 
-export default defineConfig(async () => {
+import { visualizer } from 'rollup-plugin-visualizer';
+import Icon from 'unplugin-icons/vite';
+
+export default defineConfig(async ({ command }) => {
+  const plugins = [
+    react(),
+    WindiCSS(),
+    Icon({ compiler: 'jsx', jsx: 'react', autoInstall: true }),
+    ssr({ prerender: { noExtraDir: true } }),
+    vercel(),
+    vercelSsr()
+  ];
+  if (process.env.ANALYZE === 'true' && command === 'build')
+    plugins.push(visualizer({ filename: './dist/analyze.html' }));
   return {
     resolve: {
       alias: {
         '~': path.resolve(__dirname, '.')
       }
     },
-    plugins: [
-      react(),
-      WindiCSS(),
-      Icon({ compiler: 'jsx', jsx: 'react', autoInstall: true }),
-      ssr({
-        prerender: {
-          noExtraDir: true
-        }
-      }),
-      vercel(),
-      vercelSsr()
-    ],
+    plugins: plugins,
     vercel: {
       expiration: 25
     },
