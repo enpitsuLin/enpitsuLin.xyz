2cab3e8ec2925edb6af5df3c2bac2cf8dea7fc3c
enpitsulin <enpitsulin@gmail.com>
2022-12-08 11:11:57 +0800

    feat: change site metadata

--- renderer/_default.page.server.tsx
+++ renderer/_default.page.server.tsx
@@ -2,7 +2,6 @@ import { renderToString } from 'react-dom/server';
 import { dangerouslySkipEscape, escapeInject } from 'vite-plugin-ssr';
 import type { PageContextBuiltIn } from 'vite-plugin-ssr/types';
 import { PageWrapper } from '~/components/PageWrapper';
-import logoUrl from './logo.svg';
 import type { PageContext } from './types';
 
 export { render };
@@ -20,15 +19,31 @@ async function render(pageContext: PageContextBuiltIn & PageContext) {
 
   // See https://vite-plugin-ssr.com/html-head
   const { documentProps } = pageContext;
-  const title = (documentProps && documentProps.title) || 'Vite SSR app';
-  const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr';
+  const title = (documentProps && documentProps.title) || "enpitsulin's blog";
+  const desc = (documentProps && documentProps.description) || 'Make things happy';
 
   const injected = escapeInject`<!DOCTYPE html>
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
-        <link rel="icon" href="${logoUrl}" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/static/favicons/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/static/favicons/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/static/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/static/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#000000" />
+          <meta name="theme-color" content="#000000" />
+          <link rel="alternate" type="application/rss+xml" href="/feed" />
         <meta name="description" content="${desc}" />
         <title>${title}</title>
       </head>
