1e853d0cff63f13fa113a87e217e4f2dc289f843
enpitsulin <enpitsulin@gmail.com>
2022-12-08 10:41:57 +0800

    feat: scroll-behavior smooth

--- components/Link.tsx
+++ components/Link.tsx
@@ -1,12 +1,21 @@
 import clsx from 'clsx';
-import { usePageContext } from '~/hooks/usePageContext';
+import { navigate } from 'vite-plugin-ssr/client/router';
 
 interface LinkProp extends React.DetailedHTMLProps<React.AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement> {}
 
 export const Link: React.FC<LinkProp> = (props) => {
-  const pageContext = usePageContext();
-  const className = [props.className, pageContext.urlPathname === props.href && 'is-active'].filter(Boolean).join(' ');
-  return <a {...props} className={className} />;
+  const { href, ...anchorProps } = props;
+  const isInternalLink = href && href.startsWith('/');
+  const isAnchorLink = href && href.startsWith('#');
+  const className = clsx([props.className, 'cursor-pointer']);
+
+  if (isInternalLink) {
+    return <a {...anchorProps} className={className} onClick={() => navigate(href)} />;
+  }
+  if (isAnchorLink) {
+    return <a {...props} className={className} />;
+  }
+  return <a target="_blank" rel="noopener noreferrer" {...props} className={className} />;
 };
 
 export const Tag: React.FC<LinkProp & { text: string }> = ({ text, className, children, ...rest }) => {
