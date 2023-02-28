ddbdcadeb105c7d6dda7dcd9f42111a854e58f80
enpitsulin <enpitsulin@gmail.com>
2023-02-28 21:57:57 +0800

    feat: adjust typography style

--- unocss.config.ts
+++ unocss.config.ts
@@ -8,6 +8,18 @@ import {
 import type { Theme } from 'unocss/preset-uno';
 
 export default defineConfig<Theme>({
-  presets: [presetUno(), presetTypography()],
+  presets: [
+    presetUno(),
+    presetTypography({
+      cssExtend: {
+        'h1,h2,h3,h4,h5,h6': {
+          'font-weight': '500'
+        },
+        'p,ul,ol,pre': {
+          'font-size': '0.9375rem'
+        }
+      }
+    })
+  ],
   transformers: [transformerDirectives(), transformerVariantGroup()]
 });
