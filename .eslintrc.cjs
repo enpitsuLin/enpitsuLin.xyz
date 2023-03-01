195dd5a2b65168668a6ec70b07a8bc477aefe090
enpitsulin <enpitsulin@gmail.com>
2023-03-01 14:07:13 +0800

    chore: add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,43 @@
+module.exports = {
+  overrides: [
+    {
+      files: ['*.astro'],
+      plugins: ['astro'],
+      env: {
+        node: true,
+        'astro/astro': true,
+        es2020: true
+      },
+      parser: 'astro-eslint-parser',
+      parserOptions: {
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro'],
+        sourceType: 'module'
+      },
+      rules: {
+        'astro/no-conflict-set-directives': 'error',
+        'astro/no-unused-define-vars-in-style': 'error'
+      }
+    },
+    {
+      files: ['**/*.astro/*.js', '*.astro/*.js'],
+      env: {
+        browser: true,
+        es2020: true
+      },
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+      parserOptions: {
+        sourceType: 'module'
+      },
+      parser: '@typescript-eslint/parser',
+      rules: {
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_', destructuredArrayIgnorePattern: '^_' }
+        ],
+        '@typescript-eslint/no-non-null-assertion': 'off'
+      }
+    }
+  ]
+};
