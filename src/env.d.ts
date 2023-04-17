69be5f00da8729f833dd4f9345cede111ff61876
enpitsulin <enpitsulin@gmail.com>
2023-04-17 23:20:45 +0800

    fix: global type definition

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,14 +1,15 @@
 /// <reference types="astro/client" />
 /// <reference types="astro/import-meta" />
 import type { AttributifyAttributes } from '@unocss/preset-attributify'
-interface Window {
-  DISQUS: {
-    reset: ({ reload: boolean }) => void;
-  };
-}
+
 
 declare global {
   namespace astroHTML.JSX {
     type HTMLAttributes = AttributifyAttributes
+  }
+  interface Window {
+    DISQUS: {
+      reset: ({ reload: boolean }) => void;
+    };
   }
 }
