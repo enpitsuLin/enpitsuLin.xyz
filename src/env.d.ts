799e65b73fe6f53826d2f277792eb630ea6350f2
enpitsulin <enpitsulin@gmail.com>
2023-04-17 22:30:45 +0800

    feat: use unocss' attributes mode

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,14 @@
 /// <reference types="astro/client" />
 /// <reference types="astro/import-meta" />
+import type { AttributifyAttributes } from '@unocss/preset-attributify'
 interface Window {
   DISQUS: {
     reset: ({ reload: boolean }) => void;
   };
+}
+
+declare global {
+  namespace astroHTML.JSX {
+    type HTMLAttributes = AttributifyAttributes
+  }
 }
