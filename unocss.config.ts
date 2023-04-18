99193ff0e7cee87b903f9c749b00f394175b1f13
enpitsulin <enpitsulin@gmail.com>
2023-04-18 17:14:43 +0800

    feat: dispaly social in hero

--- unocss.config.ts
+++ unocss.config.ts
@@ -10,6 +10,14 @@ import {
 import type { Theme } from 'unocss/preset-uno';
 
 export default defineConfig<Theme>({
+  shortcuts: [
+    {
+      'lodash-effect': [
+        'relative after:absolute after:content-empty after:h-1 after:w-full after:bottom-0 after:left-0 ',
+        'after:scale-0 hover:after:scale-100 after:transition-transform after:origin-l'
+      ].join(' ')
+    }
+  ],
   presets: [
     presetUno(),
     presetAttributify(),
