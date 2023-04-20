5861c7ef92ad93d2cb7dbc8260b6fbefc523828d
enpitsulin <enpitsulin@gmail.com>
2023-04-20 23:56:14 +0800

    feat: use component with hastscript

---
+++ src/lib/hastscript-to-rehype-plugin.ts
@@ -0,0 +1,43 @@
+import type { ElementContent, Root, RootContent } from "hast";
+import type { HResult } from "hastscript/lib/core";
+import type { Plugin } from "unified";
+import { SKIP, visit } from 'unist-util-visit';
+
+export interface HProps {
+  children: ElementContent[]
+  [attribute: string]: any
+}
+export type HComponent = (props: HProps) => HResult
+
+interface Options {
+  components?: Record<string, HComponent>
+}
+/** 
+ * @example
+ * ```
+ * const SomeComponent: HComponent = (p) => h('div', { class: 'some-class' }, ...p.children)
+ * # use it 
+ * rehypeCustomElement({components:{'some-component':SomeComponent}})
+ * # then in markdown file
+ * "<some-component></some-component>"
+ * ```
+ */
+const rehypeCustomElement: Plugin<[Options], Root> =
+  ({ components = {} }) => {
+    return (tree, _vfile) => {
+      visit(tree, (node, index, parent) => {
+        if (node.type !== 'element' || !parent) return
+        if (node.tagName in components) {
+          const renderToComponent = components[node.tagName]
+          const replacedNode = renderToComponent({
+            ...node.properties ?? {},
+            children: node.children
+          })
+          parent.children[index!] = replacedNode as RootContent;
+          return [SKIP, index];
+        }
+      })
+    }
+  }
+
+export default rehypeCustomElement
