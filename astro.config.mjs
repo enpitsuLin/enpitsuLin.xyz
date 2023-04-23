f73633e9fba1c5768e76efc7166d1c5c5ec4ec4b
enpitsulin <enpitsulin@gmail.com>
2023-04-23 13:55:22 +0800

    feat: add markdown plugins

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,80 @@
-import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
 import Unocss from 'unocss/astro';
 
 // https://astro.build/config
 import solidJs from '@astrojs/solid-js';
 
+
+import rehypePrismDiff from 'rehype-prism-diff';
+import rehypePrismPlus from 'rehype-prism-plus';
+import rehypeRewrite from 'rehype-rewrite';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkDirective from 'remark-directive';
+import remarkDirectiveRehype from 'remark-directive-rehype';
+import remarkCodeTitles from 'remark-flexible-code-titles';
+import rehypeSlug from 'rehype-slug';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://enpitsulin.xyz',
-  integrations: [mdx(), sitemap(), Unocss(), solidJs()]
+  integrations: [mdx(), sitemap(), Unocss(), solidJs()],
+  markdown: {
+    gfm: true,
+    syntaxHighlight: false,
+    remarkPlugins: [
+      [
+        remarkCodeTitles, {
+          title: false,
+          containerProperties: (lang, title) => ({
+            ['data-language']: lang,
+            ['data-title']: title
+          })
+        }
+      ],
+      remarkDirective, remarkDirectiveRehype],
+    rehypePlugins: [
+      rehypePrismPlus, rehypePrismDiff, rehypeSlug,
+      [rehypeAutolinkHeadings, {
+        behavior: 'append',
+        properties: {
+          ariaHidden: 'true',
+          tabIndex: -1,
+          class: 'absolute -left-6'
+        },
+        content: {
+          type: 'element',
+          tagName: 'span',
+          properties: {
+            className: ['op-0', 'group-hover:op-100']
+          },
+          children: [
+            {
+              type: 'text',
+              value: '#'
+            }
+          ]
+        }
+      }],
+      [rehypeRewrite, {
+        rewrite: (node) => {
+          if (node.type === 'element' && node.tagName === 'img') {
+            node.properties = Object.assign(node.properties || {}, {
+              'data-zoomable': true,
+              loading: 'lazy'
+            });
+          }
+          if (
+            node.type === 'element' &&
+            ['h2', 'h3', 'h4', 'h5', 'h6'].includes(node.tagName)
+          ) {
+            node.properties = Object.assign(node.properties || {}, {
+              className: 'group'
+            });
+          }
+        }
+      }],
+    ]
+  }
 });
