8785a4bbdb81bd5eb66cebccbbbed0ddea899b80
enpitsulin <enpitsulin@gmail.com>
2023-07-12 11:12:47 +0800

    fix: animation theme extends

--- unocss.config.ts
+++ unocss.config.ts
@@ -13,9 +13,9 @@ export default defineConfig<Theme>({
     (theme) => {
       const animation: Theme['animation'] = Object.assign(theme.animation ?? {}, {
         keyframes: {
-          'blink-text-cursor': '{from{border-right-color: currentColor;}to{border-right-color:transparent;}}'
+          'blink-text-cursor': '{from{border-right-color: currentColor;}to{border-right-color:transparent;}}',
+          'wave-hands': `{0%{transform:rotate(0.0deg)}10%{transform:rotate(14.0deg)}20%{transform:rotate(-8.0deg)}30%{transform:rotate(14.0deg)}40%{transform:rotate(-4.0deg)}50%{transform:rotate(10.0deg)}60%{transform:rotate(0.0deg)}100%{transform:rotate(0.0deg)}}`
         },
-        'wave-hands': `{0%{transform:rotate(0.0deg)}10%{transform:rotate(14.0deg)}20%{transform:rotate(-8.0deg)}30%{transform:rotate(14.0deg)}40%{transform:rotate(-4.0deg)}50%{transform:rotate(10.0deg)}60%{transform:rotate(0.0deg)}100%{transform:rotate(0.0deg)}}`
       })
 
       return { ...theme, animation }
