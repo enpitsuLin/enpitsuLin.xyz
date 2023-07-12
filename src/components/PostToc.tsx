70b35db3a95873b7e073fc7724fbe65bd98358a7
enpitsulin <enpitsulin@gmail.com>
2023-07-12 14:20:02 +0800

    style: prettier

--- src/components/PostToc.tsx
+++ src/components/PostToc.tsx
@@ -17,105 +17,109 @@ function getIds(items: TocResult['map']) {
 }
 
 function useActiveId(itemIds: string[]) {
-  const [activeId, setActiveId] = createSignal(``)
+  const [activeId, setActiveId] = createSignal(``);
   createEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            setActiveId(entry.target.id)
+            setActiveId(entry.target.id);
           }
-        })
+        });
       },
-      { rootMargin: `0% 0% -90% 0%` },
-    )
+      { rootMargin: `0% 0% -90% 0%` }
+    );
     itemIds.forEach((id) => {
-      const element = document.getElementById(id)
+      const element = document.getElementById(id);
       if (element) {
-        observer.observe(element)
+        observer.observe(element);
       }
-    })
+    });
     return () => {
       itemIds.forEach((id) => {
-        const element = document.getElementById(id)
+        const element = document.getElementById(id);
         if (element) {
-          observer.unobserve(element)
+          observer.unobserve(element);
         }
-      })
-    }
-  }, [itemIds])
-  return activeId
+      });
+    };
+  }, [itemIds]);
+  return activeId;
 }
 
-function renderItems(items: TocResult["map"], activeId: string, prefix = "") {
+function renderItems(items: TocResult['map'], activeId: string, prefix = '') {
   return (
-    <ol class="overflow-y-auto max-h-65vh" classList={{ "pl-5": prefix != '' }}>
+    <ol class="overflow-y-auto max-h-65vh" classList={{ 'pl-5': prefix != '' }}>
       {items?.children?.map((item, index) => (
         <li>
           {item.children.map((child: any) => {
             const children =
               child.children[0].children?.[0]?.children ||
-              child.children[0].children
-            const content = (children as { value: string }[]).reduce((acc, curr) => acc + curr.value, "");
+              child.children[0].children;
+            const content = (children as { value: string }[]).reduce(
+              (acc, curr) => acc + curr.value,
+              ''
+            );
             return (
               <span>
-                {child.type === "paragraph" && child.children?.[0]?.url && (
+                {child.type === 'paragraph' && child.children?.[0]?.url && (
                   <a
                     href={child.children[0].url}
                     title={content}
                     aria-hidden={activeId !== child.children[0].url.slice(1)}
-                    class={
-                      `${(activeId === child.children[0].url.slice(1)
-                        ? "text-neutral-700 dark:text-neutral "
-                        : "text-neutral dark:text-neutral-700")
-                      } truncate inline-block max-w-full align-bottom hover:text-neutral`
-                    }
+                    class={`${
+                      activeId === child.children[0].url.slice(1)
+                        ? 'text-neutral-700 dark:text-neutral '
+                        : 'text-neutral dark:text-neutral-700'
+                    } truncate inline-block max-w-full align-bottom hover:text-neutral`}
                   >
                     {`${prefix}${index + 1}. ${content}`}
                   </a>
                 )}
-                {child.type === "list" &&
+                {child.type === 'list' &&
                   renderItems(child, activeId, `${index + 1}.`)}
               </span>
-            )
+            );
           })}
         </li>
       ))}
     </ol>
-  )
+  );
 }
 
 export const PostToc: Component<{ data: TocResult }> = (props) => {
+  let containerRef: HTMLDivElement | undefined;
 
-  let containerRef: HTMLDivElement | undefined
-
-  const [maxWidth, setMaxWidth] = createSignal(0)
+  const [maxWidth, setMaxWidth] = createSignal(0);
 
   createEffect(() => {
     if (containerRef) {
       setMaxWidth(
-        (window.innerWidth -
-          (containerRef?.parentElement?.clientWidth || 0)) / 2
-        -
-        40,
-      )
+        (window.innerWidth - (containerRef?.parentElement?.clientWidth || 0)) /
+          2 -
+          40
+      );
     }
-  })
+  });
 
-  const idList = getIds(props.data.map)
-  const activeId = useActiveId(idList)
+  const idList = getIds(props.data.map);
+  const activeId = useActiveId(idList);
 
   return (
     <div
       ref={containerRef}
       class="absolute left-full pl-10 h-full top-0"
-      classList={{ "hidden lg:block": maxWidth() > 40, 'hidden': maxWidth() < 40 }}
+      classList={{
+        'hidden lg:block': maxWidth() > 40,
+        hidden: maxWidth() < 40
+      }}
       style={{
-        "max-width": maxWidth() > 40 ? maxWidth() + 'px' : 0,
-      }}>
+        'max-width': maxWidth() > 40 ? maxWidth() + 'px' : 0
+      }}
+    >
       <div class="sticky top-14 text-sm truncate leading-loose">
         {renderItems(props.data?.map, activeId())}
       </div>
     </div>
-  )
-}
+  );
+};
