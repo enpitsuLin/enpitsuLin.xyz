b27585eaadc775656cfc2a414182ad25dfe86b88
enpitsulin <enpitsulin@gmail.com>
2023-08-07 11:03:42 +0800

    feat: use component to load medium-zoom

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'enpitsulin';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'Keep young keep simple';
 export const GITHUB_PROFILE = 'https://www.github.com/enpitsulin';
 
 export const ITEMS_DISPLAY_INDEX = 3
